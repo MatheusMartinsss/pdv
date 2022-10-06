f140a3bf99db1e1de00e733c74a3290dd83747cd
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-06 15:56:32 -0300

    create base theme

---
+++ src/main/factories/themes/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const Theme = createTheme({
+    palette: {
+      primary: {
+        main: '#FFFFFF'
+      },
+      secondary: {
+        main: '#8DBDFF'
+      }
+    }
+})
--- src/main/routers/Routers.tsx
+++ src/main/routers/Routers.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MakeHome, MakeAuth } from '../factories/pages';
+import { ThemeProvider } from '@emotion/react';
+import './Routers.css'
+import { Theme } from '../factories/themes/theme';
 const Routers: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/auth' element={<MakeAuth />} />
-        <Route path='/' element={<MakeHome />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/auth' element={<MakeAuth />} />
+          <Route path='/' element={<MakeHome />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
