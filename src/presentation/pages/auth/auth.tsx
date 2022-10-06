2535832cfbf5a94b2a2776f26733cc2ea4808f57
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-06 15:56:51 -0300

    create auth screen

--- src/presentation/pages/auth/auth.tsx
+++ src/presentation/pages/auth/auth.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { Container } from '@mui/material'
-
-const Auth = () => {
+import { Box, Paper, TextField, Divider, Button, Typography } from '@mui/material'
+import Logo from '../../components/logo/logo'
+const Auth: React.FC = () => {
     return (
-        <Container>
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems='center'
+            minHeight="100vh"
+        >
+            <Box
+                component={Paper}
+                elevation={4}
+                gap={2}
+                sx={{
+                    display: 'flex',
+                    justifyContent: "center",
 
-        </Container>
+                    flexDirection: 'column',
+                    maxWidth: '400px',
+                    padding: '40px',
+                    flexGrow: '1'
+                }}
+            >
+                <Logo />
+                <Divider >
+                    <Typography variant='h6'>Bem-Vindo</Typography>
+                </Divider>
+                <TextField fullWidth label='Usuário'></TextField>
+                <TextField fullWidth label='Senha' type='password'></TextField>
+                <Button color='secondary' fullWidth variant='contained'>Entrar</Button>
+            </Box>
+        </Box>
     )
 }
 export default Auth
