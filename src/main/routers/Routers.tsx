83ffff070029548e0ebadb77466f0f085f849dea
MatheusMartinsss <matheus.s.martins98@gmail.com>
2022-10-06 09:46:46 -0300

    refactor make home factory folder

--- src/main/routers/Routers.tsx
+++ src/main/routers/Routers.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { MakeHome } from '../factories';
+import { MakeHome } from '../factories/pages';
 const Routers: React.FC = () => {
   return (
     <BrowserRouter>
